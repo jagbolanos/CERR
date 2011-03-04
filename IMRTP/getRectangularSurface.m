1f35366ab7de4ba29a17a9d50c675a3994e5f439
Jorge <jagbolanos@jagbolanos-laptop.(none)>
2011-03-04 15:25:26 -0600

    using image parameters, there is an error on the field size

--- IMRTP/getRectangularSurface.m
+++ IMRTP/getRectangularSurface.m
@@ -1,19 +1,37 @@
-function [nrows, ncols, nslices] = getRectangularSurface(cols, slices)
-%cols = 102; % in this particular case FieldSize / 0.0977 right now an even number is necessary
-%slices = 40; %in this particular case FieldSize / 0.25 right now an even number is necessary
-%edgeS.rows = repmat(256,1,squareFieldSizeCols*squareFieldSizeSlices);
-%edgeS.cols = repmat((256-squareFieldSizeCols/2+1):(256+squareFieldSizeCols/2),1,squareFieldSizeSlices); % 512 / 2 => 256
-%edgeS.slices = repmat((64-squareFieldSizeSlices/2+1):(64+squareFieldSizeSlices/2),1,squareFieldSizeCols) %1
+function [nrows, ncols, nslices] = getRectangularSurface(IM, planC)
+indexS = planC{end};
+
+%convert the field from xz plante to cols vs slices
+cols = round(IM.beams.xFieldSize / planC{indexS.scan}.uniformScanInfo.grid1Units);
+slices = round(IM.beams.zFieldSize / planC{indexS.scan}.uniformScanInfo.sliceThickness);
+
+%adjust for calculations
+if mod(cols,2) == 1
+    cols = cols + 1;
+end
+
+if mod(slices,2) == 1
+    slices = slices + 1;
+end
+
+%get the isocenter so we can place the field at it
+[misocenter.r misocenter.c misocenter.s] = xyztom([IM.beams.isocenter.x],[IM.beams.isocenter.y],[IM.beams.isocenter.z],1,planC,'uniform');
+misocenter.r = round(misocenter.r);
+misocenter.c = round(misocenter.c);
+misocenter.s = round(misocenter.s);
 
 ncols = zeros(1,cols*slices);
 i = 1;
-for v=(256-cols/2+1):(256+cols/2)
+for v=(misocenter.c-cols/2+1):(misocenter.c+cols/2)
     ncols(i:i+slices-1) = repmat(v,1,slices);
     i = i + slices;
 end
 
-nslices = repmat((64-slices/2+1):(64+slices/2),1,cols);
-nrows = repmat(256,1,cols*slices);
-%scatter(ncols,nslices);
+%for each vector of cols associate with the corresponding slices
+nslices = repmat((misocenter.s-slices/2+1):(misocenter.s+slices/2),1,cols);
+
+%the row is fixed as it is a plane along cols vs. slices
+nrows = repmat(misocenter.r,1,cols*slices);
+
 
 end
--- IMRTP/updateIMRTP.m
+++ IMRTP/updateIMRTP.m
@@ -121,7 +121,7 @@ edgeS = getTargetSurfacePoints(IM);
 [structROIV, sampleRateV] = getROIStructureList(IM);
 
 disp('using the modified surface edge')
-[edgeS.rows edgeS.cols edgeS.slices] = getRectangularSurface(50, 20);
+[edgeS.rows edgeS.cols edgeS.slices] = getRectangularSurface(IM, planC);
 
 %Set PB vectors, determine which PBs are required to cover the target.
 IM = getPBList(IM, edgeS);
